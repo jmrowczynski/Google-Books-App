32abeea1b0728cd9e653c89757b46e69ade21afe
Jakub <jmrowczynski12@gmail.com>
2019-09-02 21:49:23 +0200

    Code refactoring

--- script.js
+++ script.js
@@ -1,93 +1,43 @@
 const app = document.getElementById('root');
 const container = document.createElement('div');
-container.setAttribute('class', 'container')
-const fragment = document.createDocumentFragment();
+container.classList.add('container');
+const btn = document.querySelector('.sbtn');
 
-console.log(document.body.scrollHeight);
-console.log(document.getElementById('root').scrollHeight);
-console.log(document.body.clientHeight);
-console.log(window.innerHeight);
-
-function search(){
-  container.innerHTML = "";
-  const pattern = document.getElementById('pattern').value;
-
-  let index = 0;
-
-  fetch(`https://www.googleapis.com/books/v1/volumes?q=${pattern}&maxResults=20&startIndex=${index}`)
-  .then(response => response.json())
-  .then(data => {
-
-    //console.log(data);
-
-    for (let i = 0; i < data.items.length;  i++) {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card')
-      const t = document.createElement('h4');
-      t.textContent = data.items[i].volumeInfo.title;
-      const image = document.createElement('img');
-      // checking if imageLinks is in object
-      if ('imageLinks' in data.items[i].volumeInfo){
-        image.src = data.items[i].volumeInfo.imageLinks.thumbnail;
-      }
-
-      const descript = document.createElement('p');
-      if ('description' in data.items[i].volumeInfo){
-        descript.textContent = data.items[i].volumeInfo.description.slice(0, 150) + '...';
-      }
-
-      card.appendChild(image);
-      card.appendChild(t);
-      card.appendChild(descript);
-      fragment.appendChild(card);
-    }
-
-    container.appendChild(fragment);
-    root.appendChild(container);
-
-  }
-  )
+const createTemplate = (data) => {
+  data.items.map(book => {
+    const { imageLinks, title, description } = book.volumeInfo;
+    const image = imageLinks && imageLinks.thumbnail || 'https://via.placeholder.com/128x192';
+    
+    container.innerHTML += 
+    `<div class="card">
+      <img src=${image}/>
+      <h4>${title}</h4>
+      <p>${(description || '').split(' ', 15).join(' ')}...</p>
+    </div>
+    `
+  });
+  app.appendChild(container);
 }
 
+const search = async () => {
+  const pattern = document.getElementById('pattern').value;
+  const index = container.children.length || 0;
+
+  const fetchData = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${pattern}&maxResults=20&startIndex=${index}`)
+  const response = await fetchData.json();
+  const data = await response;
+
+  return createTemplate(data);
+}
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  container.innerHTML = '';
+  search();
+});
+
 document.addEventListener('scroll', () => {
-  if (document.body.scrollHeight - document.body.scrollTop == window.innerHeight && document.getElementsByClassName('card').length > 0){
-    index = document.getElementsByClassName('card').length;
-    const pattern = document.getElementById('pattern').value;
-
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${pattern}&maxResults=20&startIndex=${index}`)
-    .then(response => response.json())
-    .then(data => {
-
-      //console.log(data);
-
-      for (let i = 0; i < data.items.length;  i++) {
-        const card = document.createElement('div');
-        card.setAttribute('class', 'card')
-        const t = document.createElement('h4');
-        t.textContent = data.items[i].volumeInfo.title;
-        const image = document.createElement('img');
-        // checking if imageLinks is in object
-        if ('imageLinks' in data.items[i].volumeInfo){
-          image.src = data.items[i].volumeInfo.imageLinks.thumbnail;
-        }
-
-        const descript = document.createElement('p');
-        if ('description' in data.items[i].volumeInfo){
-          descript.textContent = data.items[i].volumeInfo.description.slice(0, 150) + '...';
-        }
-
-        card.appendChild(image);
-        card.appendChild(t);
-        card.appendChild(descript);
-        fragment.appendChild(card);
-      }
-
-      container.appendChild(fragment);
-      root.appendChild(container);
-
-    }
-    )
-
+  if(container.getBoundingClientRect().bottom - 200 < window.innerHeight){
+    search();
   }
-
 })
